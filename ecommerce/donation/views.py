08e7e8fbbd0fcd83d6603c934dc2e3eba6e405a6
DanielsonSilva <danielson_fx@yahoo.com.br>
2024-05-13 21:16:19 -0300

    Iniciando estudos sobre pagamento no stripe

---
+++ ecommerce/donation/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+import stripe
+from django.urls import reverse
+
+from ecommerce import settings
+
+
+def my_donation(request):
+    stripe.api_key = settings.STRIPE_PRIVATE_KEY
+    session = stripe.checkout.Session.create(
+        line_items=[{
+            'price': 'price_1PG6eZEShB3ZT1fJRi0EonFw',
+            'quantity': 1
+        }],
+        mode='payment',
+        success_url=request.build_absolute_uri(reverse('payment-success')) + '?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=request.build_absolute_uri(reverse('payment-failed')),
+    )
+    return render(request, 'donation/my-donation.html')
+
+
+def payment_success(request):
+    return render(request, 'donation/payment-success.html')
+
+
+def payment_failed(request):
+    return render(request, 'donation/payment-failed.html')
